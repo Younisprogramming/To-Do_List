fef1a9e1d60c4de92c5a25d0139f627cb2d7cfdc
Younisprogramming <younistaher12341@gmail.com>
2024-09-24 04:47:39 -0700

    fix the redirect from pages

--- web_flask/routes.py
+++ web_flask/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from web_flask import app, db, bcrypt
 from web_flask.forms import RegistrationForm, LoginForm
 from web_flask.models import User, Todo
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -28,7 +28,7 @@ def home():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('dashboard'))
 
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -44,14 +44,14 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('dashboard'))
 
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            return redirect(url_for('dashboard'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -59,9 +59,13 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('register'))
 
 @app.route("/dashboard")  
 def dashboard():
     return render_template('dashboard.html', title='Dashboard')
+
+@app.route("/account")
+def account():
+    return render_template('account.html', title='Account')
     
